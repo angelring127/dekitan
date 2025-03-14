112c13ab1d135a660002d3a28e7240f755955513
katayama <katayama.k@maebe.jp>
2025-03-14 13:38:20 +0900

    Add: 導入画面実装

--- src/app/tutorial/init-page/page.tsx
+++ src/app/tutorial/init-page/page.tsx
@@ -12,12 +12,13 @@ export default function InitPage() {
   const [showFinalPanel, setShowFinalPanel] = useState(false)
 
   const handleNext = () => {
-    if (currentIndex < items.length - 1) {
+    if (currentIndex < items.length - 1 && currentIndex !== 2) {
       setCurrentIndex((prev) => prev + 1)
     }
   }
 
   const handleOmikuji = () => {
+    setCurrentIndex(currentIndex + 1)
     setShowOmikuji(true)
   }
 
@@ -47,48 +48,67 @@ export default function InitPage() {
 
   if (showOmikuji) {
     return (
-      <div className="mx-auto flex h-[844px] w-[390px] flex-col items-center overflow-hidden bg-[url('/images/messages/bg_message.png')] bg-cover bg-center bg-no-repeat">
-        <div className="relative flex w-full flex-1 flex-col items-center justify-center">
+      <div className="mx-auto flex h-[844px] flex-col items-center bg-[url('/images/messages/bg_message.png')] bg-cover bg-center bg-no-repeat">
+        <div className="relative flex w-full flex-1 flex-col items-center justify-center overflow-y-auto">
           <div
             className={`${
               showResult
-                ? 'animate-shrink-and-move z-0'
-                : 'animate-shake-infinite cursor-pointer z-10'
+              ? 'animate-shrink-and-move z-0'
+              : 'animate-shake-infinite cursor-pointer z-10'
             }`}
             onClick={handleOmikujiClick}
             role="button"
             tabIndex={0}
             aria-label="おみくじをタップ"
-          >
+            >
             <Image
               src="/images/img_omikuji.png"
               alt="おみくじ"
               width={300}
               height={300}
               className="mx-auto"
-            />
+              />
           </div>
           <div
-            className={`absolute inset-0 flex flex-col items-center justify-center gap-8 ${
+            className={`absolute inset-0 flex flex-col items-center justify-center gap-8 overflow-y-auto ${
               !showResult ? 'pointer-events-none' : 'z-20'
             }`}
           >
             {showResult && (
-              <div className="animate-grow-and-center">
+              <>
+                <div className="animate-grow-and-center">
+                  <Image
+                    src="/images/img_result.png"
+                    alt="おみくじ結果"
+                    width={450}
+                    height={450}
+                    className="mx-auto"
+                  />
+                </div>
                 <Image
-                  src="/images/img_result.png"
+                  src="/images/img_little_girl_2.png"
                   alt="おみくじ結果"
-                  width={450}
-                  height={450}
-                  className="mx-auto"
+                  width={80}
+                  height={80}
+                  className="mx-auto absolute animate-fade-in-up"
+                  style={{ top: '5%', left:'5%' }}
                 />
-              </div>
+                <Image
+                  src="/images/img_little_girl.png"
+                  alt="おみくじ結果"
+                  width={80}
+                  height={80}
+                  className="mx-auto absolute animate-fade-in-up"
+                  style={{ top: '25%', right:'5%' }}
+                />
+              </>
             )}
             {showFinalPanel && (
-              <div className="animate-fade-in-up w-[280px]">
+              <div className="animate-fade-in-up w-[280px] mb-3">
                 <InformationPanel
-                  items={[items[items.length - 1]]}
-                  currentIndex={0}
+                  items={items}
+                  onNext={handleNext}
+                  currentIndex={currentIndex}
                   background="transparent"
                   withShadow
                   style={commonPanelStyle}
@@ -102,16 +122,16 @@ export default function InitPage() {
   }
 
   return (
-    <div className="mx-auto flex h-[844px] w-[390px] items-center justify-center bg-[url('/images/messages/bg_message.png')] bg-cover bg-center bg-no-repeat">
+    <div className="mx-auto flex h-[844px] items-center justify-center bg-[url('/images/messages/bg_message.png')] bg-cover bg-center bg-no-repeat">
       <div className="flex h-full w-full items-center justify-center px-4">
         <InformationPanel
-          items={items.slice(0, -1)}
+          items={items}
           currentIndex={currentIndex}
           onNext={handleNext}
           background="transparent"
           withShadow
           style={commonPanelStyle}
-          className="w-full max-w-[320px] flex flex-col h-[75%]"
+          className="w-full max-w-[320px] flex flex-col h-[75%] animate-scroll"
         />
       </div>
     </div>

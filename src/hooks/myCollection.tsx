12e45781e2513f415ee2a25fd4fa3413b8bed3da
katayama <katayama.k@maebe.jp>
2025-04-09 13:07:52 +0900

    Update: 調整

--- src/hooks/myCollection.tsx
+++ src/hooks/myCollection.tsx
@@ -2,32 +2,31 @@ import type { InformationItem } from '@/components/common/InformationPanel/types
 import type { CollectionItem } from '@/types/info'
 
 export const myCollection = (singleCollectionItem: CollectionItem | null) => {
-  const items: InformationItem[] = [
-    {
-      id: '0',
-      content: (
-        <div className="flex flex-col items-center gap-4">
-          <span className="whitespace-pre-line text-center text-1xl font-bold">
-            ひとつのコレクションが<br></br>ふえた!
-          </span>
-        </div>
-      ),
-    },
-    {
-      id: '1',
-      content: (
-        <div className="flex flex-col items-center gap-4">
-          <span className="whitespace-pre-line text-center text-2xl font-bold">
-            {singleCollectionItem?.title}
-          </span>
-          <span
-            className="whitespace-pre-line text-center text-xl font-bold"
-            dangerouslySetInnerHTML={{ __html: singleCollectionItem?.description || '' }}
-          />
-        </div>
-      ),
-    },
-  ]
+    const items: InformationItem[] = [
+        {
+            id: '0',
+            content: (
+                <div className="flex flex-col items-center gap-4">
+                    <span className="whitespace-pre-line text-center text-1xl font-bold">
+                        ひとつのコレクションが<br></br>ふえたね!
+                    </span>
+                </div>
+            ),
+        },
+        {
+            id: '1',
+            content: (
+                <div className="flex flex-col items-center gap-4 w-[80%]" style={{marginTop: "50px"}}>
+                    <span className="whitespace-pre-line text-center text-2xl font-bold">
+                        {singleCollectionItem?.title}
+                    </span>
+                    <span
+                        className="whitespace-pre-line text-center text-xl font-bold"
+                        dangerouslySetInnerHTML={{ __html: singleCollectionItem?.description || "", }} />
+                </div>
+            ),
+        }
+    ];
 
   return {
     items,

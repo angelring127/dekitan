4f487eb453f133e8d9bb18cabd10f5736117c037
katayama <katayama.k@maebe.jp>
2025-04-21 17:40:22 +0900

    Update: デザイン反映

--- src/app/award/collect/page.tsx
+++ src/app/award/collect/page.tsx
@@ -13,17 +13,10 @@ export default function MyCollections() {
   const { mycollection, addToCollection, setSingleCollectionItem, singleCollectionItem } = useGlobalStore();
   const volatileToken = useAuthStore.getState().token
   const playerId = useGlobalStore.getState().playerId
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalPage, setTotalPage] = useState(1)
+  const ITEMS_PAR_PAGE = 15
 
-  const handleNext = (item: CollectionItem) => {
-    setSingleCollectionItem(item);
-    setCurrentIndex((prev) => prev + 1);
-  };
-
-  const handlePrevious = () => {
-    setCurrentIndex((prev) => prev - 1);
-  };
-
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
   const hasApiBeenCalled = useRef(false);
   useEffect(() => {
     hasApiBeenCalled.current = true;
@@ -33,117 +26,201 @@ export default function MyCollections() {
         player_id: playerId,
       })
       .then((response) => {
-        console.log("response")
-        console.log(response)
         addToCollection(response.data.data.list);
+        setTotalPage(Math.ceil(mycollection.length / ITEMS_PAR_PAGE))
       })
       .catch((error) => {
         console.error('Error:', error);
       });
   }, []);
 
-  const [initialItems, setInitialItems] = useState(() => myCollection(singleCollectionItem));
-
-  useEffect(() => {
-    setInitialItems(myCollection(singleCollectionItem));
-  }, [singleCollectionItem]);
-
-  const items = initialItems?.items || [];
-
-  let buttonText = "";
-  let classname = "";
-  let style = {};
-
-  if (currentIndex === 0) {
-    buttonText = "ホームにもどる";
-    classname = 'relative h-[700px] w-[390px] mt-10  bg-cover bg-center bg-no-repeat mt-auto mb-4';
-    style = {
-      backgroundImage: `url('/images/img_cuppord.png')`,
-    };
-
-  } else if (currentIndex === 1) {
-    buttonText = "もどる";
-    classname = 'relative w-[390px] mt-auto mb-4';
-    style = {
-      background: `transparent`,
-    };
+  const selectPage = (type: string) => {
+    if(type == 'prev') {
+      setCurrentPage(currentPage - 1)
+    } else {
+      setCurrentPage(currentPage + 1)
+    }
   }
+
+  const [isOpen, setIsOpen] = useState(false)
+  const dialogRef = useRef<HTMLDialogElement>(null)
+
+  const selectCollection = (item) => {
+    setSingleCollectionItem(item)
+    setIsOpen(true)
+  }
+
+  useEffect((): void => {
+    const dialogElement = dialogRef.current
+    if (!dialogElement) {
+      return
+    }
+    if (isOpen) {
+      if (dialogElement.hasAttribute('open')) {
+        return
+      }
+      dialogElement.showModal()
+    } else {
+      if (!dialogElement.hasAttribute('open')) {
+        return
+      }
+      dialogElement.close()
+    }
+  }, [isOpen])
+
   return (
-    <div
-      className={`mx-auto flex h-[844px] w-[390px] flex-col items-center overflow-hidden ${currentIndex === 1 ? 'bg-cover bg-center bg-no-repeat' : ''}`}
-      style={currentIndex === 1 ? { backgroundImage: `url('/images/messages/bg_message.png')` } : { background: 'white' }}
-    >
-      <div className={classname} style={style}>
-        {currentIndex === 0 && (
-          <>
-            {mycollection && mycollection.length > 0 && (
-              // <div className="grid grid-cols-3 gap-4 mt-10">
-              <div className="flex flex-wrap justify-center gap-4 mt-10">
-                {mycollection.map((item) => (
-                  <div key={item?.id ?? item?.title} className="relative cursor-pointer">
-                    <Image
-                      src={item?.image ?? './images/default-image.png'}
-                      alt="award"
-                      width={150}
-                      height={100}
-                      onClick={() => handleNext(item)}
-                      className="collection_self"
-                      style={{ bottom: '-50%', left: '10%' }}
-                    />
-                  </div>
-                ))}
-              </div>
-            )}
-
-            <Image
-              src="/images/img_little_collection_animal.png"
-              alt="little_rat"
-              width={140}
-              height={140}
-              className="absolute z-10"
-              style={{ bottom: '0%', right: '1%' }}
-            />
-
-          </>
+    <div className='mx-auto flex h-[844px] w-[390px] flex-col items-center overflow-hidden bg-cover bg-center bg-no-repeat bg-[url("/images/collect/bg_collection_room.png")] relative'>
+      <div className="w-[90%]">
+        {mycollection && mycollection.length > 0 && (
+          <div className="grid grid-cols-3 gap-4 mt-10 mb-5">
+            {mycollection
+              .slice((currentPage - 1) * ITEMS_PAR_PAGE, currentPage * ITEMS_PAR_PAGE)
+              .map((item) => (
+                <div key={item?.id ?? item?.title} className="relative cursor-pointer w-full aspect-square">
+                  <Image
+                    src="/images/collect/bg_circle.png"
+                    alt="circle"
+                    width={150}
+                    height={100}
+                    onClick={() => selectCollection(item)}
+                    style={{
+                      position: "absolute",
+                      width: "100%",
+                      height: "auto",
+                    }}
+                  />
+                  <Image
+                    src={item?.image ? process.env.NEXT_PUBLIC_API_URL + item?.image : ""}
+                    alt="award"
+                    width={150}
+                    height={100}
+                    onClick={() => selectCollection(item)}
+                    style={{ 
+                      position: "absolute",
+                      top: "50%",
+                      left: "50%",
+                      transform: "translate(-50%, -50%)",
+                      width: "70%",
+                      height: "auto",
+                      margin: "auto",
+                    }}
+                  />
+                </div>
+            ))}
+          </div>
         )}
-        <div className="absolute w-full flex flex-col items-center mb-20 mt-20" style={{ bottom: '0%' }}>
-          {currentIndex === 1 && (
-            <Image
-              src={singleCollectionItem?.image ?? '/images/default-image.png'}
-              alt="award"
-              width={150}
-              height={150}
-              className="gem_points absolute animate-fade-in-up"
-              style={{ top: '-25%' }}
-            />
-          )}
-          <InformationPanel
-            items={items.slice(0)}
-            currentIndex={currentIndex}
-            background="transparent"
-            withShadow
-            className="my-4 w-[350] w-[80%] mb-120 flex flex-col items-center justify-center common_panel_style"
+
+        <dialog ref={dialogRef} className="max-w-[430px] w-[80%] relative overflow-visible bg-transparent border-none">
+          <Image
+            src="/images/collect/close_icon.png"
+            width={100}
+            height={100}
+            alt="card"
+            style={{
+              position: "absolute",
+              top: "-10%",
+              right: "0%",
+              width: "15%",
+              height: "auto",
+            }}
+            className="cursor-pointer"
+            onClick={() => setIsOpen(false)}
+          />
+          <Image
+            src="/images/collect/card.png"
+            width={100}
+            height={100}
+            alt="card"
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          />
+          <div 
+            style={{
+              position:"absolute",
+              color:"#b53922",
+              fontSize: "25px",
+              fontWeight:"bold",
+              top: "11%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: "100%",
+              textAlign: "center"
+            }}
           >
-            {currentIndex === 1 && (
-              <Button
-                className="text-lg font-bold mt-4 text-black bg-gray-300 rounded-l-full rounded-r-full"
-                onClick={handlePrevious}
-              >
-                {buttonText}
-              </Button>
-            )}
-          </InformationPanel>
+            {singleCollectionItem?.title}
+          </div>
+          <Image
+            src={singleCollectionItem?.image ? process.env.NEXT_PUBLIC_API_URL + singleCollectionItem?.image : ""}
+            width={100}
+            height={100}
+            alt="card_item"
+            style={{
+              position: "absolute",
+              top: "40%",
+              left: "50%",
+              width: "70%",
+              height: "auto",
+              transform: "translate(-50%, -50%)",
+            }}
+          />
+          <div
+            className="font-db"
+            style={{
+              position:"absolute",
+              color:"#000000",
+              bottom: "9%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              zIndex: 1,
+              width: "75%",
+            }}
+            dangerouslySetInnerHTML={{ __html: singleCollectionItem?.description ?? '' }}
+          />
+          <Image
+            src="/images/collect/card_txt_frame.png"
+            width={100}
+            height={100}
+            alt="card_text"
+            style={{
+              position: "absolute",
+              top: "80%",
+              left: "50%",
+              width: "85%",
+              height: "auto",
+              transform: "translate(-50%, -50%)",
+            }}
+          />
+        </dialog>
+
+        <div className="flex justify-center gap-6">
+          {/* 左矢印 */}
+          <div
+            className={`bg-[#00803a] text-white px-7 text-bold text-[25px] rounded-sm cursor-pointer ${
+              currentPage > 1 ? "" : "invisible"
+            }`}
+            onClick={() => currentPage > 1 && selectPage('prev')}
+          >
+            {"<"}
+          </div>
+
+          {/* 現在ページ */}
+          <div className="border-[3px] border-[#00803a] rounded-2xl bg-white py-2 px-7">
+            {currentPage} / {totalPage}
+          </div>
+
+          {/* 右矢印 */}
+          <div
+            className={`bg-[#00803a] text-white px-7 text-bold text-[25px] rounded-sm cursor-pointer ${
+              currentPage < totalPage ? "" : "invisible"
+            }`}
+            onClick={() => currentPage < totalPage && selectPage('next')}
+          >
+            {">"}
+          </div>
         </div>
       </div>
-      {currentIndex === 0 && (
-        <a href="/room">
-          <Button
-            className="text-lg font-bold relative mt-4 text-black bg-gray-300 rounded-l-full rounded-r-full"
-          >
-            {buttonText}
-          </Button>
-        </a>
-      )}
     </div>
   )
 }
